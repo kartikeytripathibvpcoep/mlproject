d8a07ebb476a58e103341346eb236996c485671a
KartikeyTripathi <kartikeytripathi990@gmail.com>
2026-01-05 07:49:46 +0530

    Hyper parameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,7 +44,50 @@ class ModelTrainer:
                 "CatBoosting_Regressor":CatBoostRegressor(verbose=False),
                 "AdaBoost_Regressor":AdaBoostRegressor(),
             }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision_Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random_Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient_Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear_Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting_Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost_Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Neighbours":{
+                    'n_neighbors':[3,5,7,9],
+                    
+                }
+                
+            }
+
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
             
             ###Taking The Best Model
             best_model_score=max(sorted(model_report.values()))
@@ -54,7 +97,7 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]            
             best_model=models[best_model_name]
-            if best_model_score<0.75:
+            if best_model_score<0.5:
                 raise CustomException("No Best model found")
             
             logging.info(f"Best model found on both training and testing dataset")
